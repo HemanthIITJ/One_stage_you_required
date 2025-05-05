7232408d808b9d8009d0c365257e0a228ec59acb
modeldeveloperai <aimodelcallmebecauseiambose@gmail.com>
2025-05-05 23:35:39 +0530

    chapter 4, 5,6

--- javascript/node_code/Modern_JavaScript/chapter_3/app.js
+++ javascript/node_code/Modern_JavaScript/chapter_3/app.js
@@ -1,4 +1,8 @@
-function fetchDataPromise() {
-  return new Promise(res => setTimeout(() => res('data1'), 100));
-}
-fetchDataPromise().then(data => fetchDataPromise().then(data2 => console.log('Promise:', data, data2)));
+
+function errorCallback(cb) {
+    setTimeout(() => cb(new Error('fail')), 100);
+  }
+  errorCallback((err, data, data1,data2) => {
+    if (err) 
+      console.error('Callback Error:', err.message);
+  });
